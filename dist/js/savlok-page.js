ed7aee0296d849d36bdf09e33d4d55de2c8a29ac
ereboz <ereboz8@gmail.com>
2017-08-15 14:14:55 -0700

    Add lok Plate Kit to Grid

--- dist/js/savlok-page.js
+++ dist/js/savlok-page.js
@@ -1,9 +1,27 @@
 // javaboxer 8-7-17
 // savlok-brochure
+
+// websites using the savlok-brochure have a bootstrap .container wrapper. We need to replace the parent container
+// with .container-fluid to accomodate full width for the brochure topic page.
+
+$(document).ready(function(){
+    
+    var loc = window.location.href.toLowerCase();
+    var pageWrapper = "";
+    
+    if (loc.indexOf('savlok-brochure') > -1) {
+        pageWrapper = $(".topic-page");
+        $(pageWrapper).parent().removeClass("container");
+        $(pageWrapper).parent().addClass("container-fluid");
+    }
+    
+});
+
+
 // make the 3 primary boxes above the fold the same height base on which box'z height is largest
 // we should add a window resize function as well
 
-$(document).ready(function(){
+$(window).load(function() {
     
     var highestBox = 0;
     var thisBox = $('div[class$="wrapper"] .box.primary');
@@ -15,4 +33,3 @@ $(document).ready(function(){
     thisBox.height(highestBox);
     
 });
-
